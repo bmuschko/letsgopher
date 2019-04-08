551a6f918908cc83b75d2d8f8e9a2db830f5eb1d
Benjamin Muschko <benjamin.muschko@gmail.com>
2019-04-08 09:05:38 -0600

    Add test for root command

---
+++ cmd/root_test.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"testing"
+)
+
+func TestRootCmd(t *testing.T) {
+	args := []string{}
+	cmd := NewRootCmd(args)
+	cmd.SetOutput(ioutil.Discard)
+	cmd.SetArgs(args)
+	cmd.Run = func(*cobra.Command, []string) {}
+	if err := cmd.Execute(); err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+}
